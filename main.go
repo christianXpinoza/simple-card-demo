d491452123a2f8f2901c9f8460f1edab433ba89f
christian espinoza <chespinoza@gmail.com>
2018-08-01 02:41:34 +0100

    add main function

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"log"
+
+	"github.com/christianXpinoza/simple-card/api"
+	"github.com/christianXpinoza/simple-card/card"
+)
+
 func main() {
+	// Create a new card service
+	cardService := card.New()
+	// New webApi service instance
+	webAPI := api.New()
+	// Start Web API
+	if err := webAPI.Start(":8080", cardService); err != nil {
+		log.Fatal(err)
+	}
 
 }
